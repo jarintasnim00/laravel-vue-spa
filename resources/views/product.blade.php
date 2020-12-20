495f7dc0c902a970d2b4faf88147d453f275c3d5
unknown <jarin35-1641@diu.edu.bd>
2020-12-20 23:30:13 +0600

    first commit

---
+++ resources/views/product.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>product</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
+</head>
+<body>
+	<br>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+	<div class="card" style="width: 18rem;">
+  <img src="images/111.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Laptop</h5>
+    <p class="card-text">HP-laptop,corei-3</p>
+    <a href="#" class="btn btn-primary">Show More</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4">
+	<div class="card" style="width: 18rem;">
+  <img src="images/222.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+  <h5 class="card-title">Asus</h5>
+    <p class="card-text">Asus-laptop,corei-3</p>
+    <a href="#" class="btn btn-primary">Show More</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4">
+	<div class="card" style="width: 18rem;">
+  <img src="images/333.jpg"  class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Laptop</h5>
+    <p class="card-text">Dell-laptop,corei-3</p>
+    <a href="#" class="btn btn-primary">Show More</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
